44b31b5fc514c218e913c8744cf4ca9a0f7c07c2
zhangyu <528420567@qq.com>
2017-03-08 22:51:47 +0800

    在工程内部建立lib文件夹放jar包，，而不是添加外部包

---
+++ Programming Assignment 2/src/Deque.java
@@ -0,0 +1,41 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> 
+{
+    public Deque()                           // construct an empty deque
+    {
+        
+    }
+    public boolean isEmpty()                 // is the deque empty?
+    {
+        
+    }
+    public int size()                        // return the number of items on the deque
+    {
+        
+    }
+    public void addFirst(Item item)          // add the item to the front
+    {
+        
+    }
+    public void addLast(Item item)           // add the item to the end
+    {
+        
+    }
+    public Item removeFirst()                // remove and return the item from the front
+    {
+        
+    }
+    public Item removeLast()                 // remove and return the item from the end
+    {
+        
+    }
+    public Iterator<Item> iterator()         // return an iterator over items in order from front to end
+    {
+        
+    }
+    public static void main(String[] args)   // unit testing (optional)
+    {
+        
+    }
+}
